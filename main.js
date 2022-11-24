1671df120f601b7f284fa79bc035229a056667a9
François Roquefort <francois.roquefort@gmail.com>
2022-11-24 15:58:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function testRegExp() {
 	let result_panel = document.getElementById("result_panel");
 	let expr_reg = new RegExp(document.getElementById("expreg_field").value);
-	if (expr_reg.test(document.getElementById("string_value").value)) {
-		result_panel.className = "ok";
-		result_panel.innerHTML = "Il y a correspondance";
-	} else {
+	try {
+		if (expr_reg.test(document.getElementById("string_value").value)) {
+			result_panel.className = "ok";
+			result_panel.innerHTML = "Il y a correspondance";
+		} else {
+			result_panel.className = "ko";
+			result_panel.innerHTML = "Pas de correspondance";
+		}
+	} catch (error) {
 		result_panel.className = "ko";
-		result_panel.innerHTML = "Pas de correspondance";
+		result_panel.innerHTML = "ERREUR DE SYNTAXE DANS L'EXPRESSION REGULIERE";
+		console.error(error);
 	}
 }
