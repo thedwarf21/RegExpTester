75e8aa16580c6c666d85a544637d9d03c06e593b
François Roquefort <francois.roquefort@gmail.com>
2022-11-24 16:17:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function testRegExp() {
 	let result_panel = document.getElementById("result_panel");
-	let expr_reg = new RegExp(document.getElementById("expreg_field").value);
 	try {
+		let expr_reg = new RegExp(document.getElementById("expreg_field").value);
 		if (expr_reg.test(document.getElementById("string_value").value)) {
 			result_panel.className = "ok";
 			result_panel.innerHTML = "Il y a correspondance";
